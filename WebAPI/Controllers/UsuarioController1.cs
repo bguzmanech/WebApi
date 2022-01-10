673850777358b15831d4a4b5e2b5bea79a6e7b62
bguzmanech <bguzmanecharri@gmail.com>
2022-01-10 00:28:00 -0300

    first commit

---
+++ WebAPI/Controllers/UsuarioController1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UsuarioController1 : ApiController
+    {
+        
+        // GET api/<controller>
+        public List<Usuario> Get()
+        {
+            return UsuarioData.Listar();
+        }
+
+        // GET api/<controller>/5
+        public Usuario Get(int id)
+        {
+            return UsuarioData.Obtener(id);
+        }
+
+        // POST api/<controller>
+        public bool Post([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Registrar(oUsuario);
+        }
+
+        // PUT api/<controller>/5
+        public bool Put([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Modificar(oUsuario);
+        }
+
+        // DELETE api/<controller>/5
+        public bool Delete(int id)
+        {
+            return UsuarioData.Eliminar(id);
+        }
+    }
+}
